e444898313a6712b5a7fa0879bf8f5650405c710
TheMajorN <nickbaranosky@gmail.com>
2023-10-22 23:15:22 -0400

    Dont need geckolib whoops

--- src/main/java/com/jakeiteasy/menagerie/client/ClientEventBusSubscriber.java
+++ src/main/java/com/jakeiteasy/menagerie/client/ClientEventBusSubscriber.java
@@ -1,4 +1,17 @@
 package com.jakeiteasy.menagerie.client;
 
+import com.jakeiteasy.menagerie.Menagerie;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Menagerie.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventBusSubscriber {
+
+    @SubscribeEvent
+    public static void onClientSetup(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        //event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
+    }
 }
--- src/main/java/com/jakeiteasy/menagerie/registry/EntityRegistry.java
+++ src/main/java/com/jakeiteasy/menagerie/registry/EntityRegistry.java
@@ -1,17 +1,23 @@
 package com.jakeiteasy.menagerie.registry;
 
 import com.jakeiteasy.menagerie.Menagerie;
+import com.jakeiteasy.menagerie.entities.AlliedVexEntity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class EntityRegistry {
 
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
             DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Menagerie.MODID);
 
-
+    public static final RegistryObject<EntityType<AlliedVexEntity>> ALLIED_VEX =
+            ENTITY_TYPES.register("rhino", () -> EntityType.Builder.of(AlliedVexEntity::new, MobCategory.CREATURE)
+                    .sized(2.5f, 2.5f).build("rhino"));
 
     public static void register(IEventBus eventBus) {
         ENTITY_TYPES.register(eventBus);
