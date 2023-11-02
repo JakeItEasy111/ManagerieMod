b3ab54720f19d765e291420391da7b5035a4954c
TheMajorN <nickbaranosky@gmail.com>
2023-11-01 23:07:46 -0400

    Animation Framework

--- src/main/java/com/jakeiteasy/menagerie/entities/AlliedVexEntity.java
+++ src/main/java/com/jakeiteasy/menagerie/entities/AlliedVexEntity.java
@@ -1,7 +1,9 @@
 package com.jakeiteasy.menagerie.entities;
 
+import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
@@ -15,6 +17,40 @@ public class AlliedVexEntity extends Allay {
     public AlliedVexEntity(EntityType<? extends Allay> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
+
+    public final AnimationState idleAnimationState = new AnimationState();
+    private int idleAnimationTimeout = 0;
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if(this.level().isClientSide()) {
+            setupAnimationStates();
+        }
+    }
+
+    private void setupAnimationStates() {
+        if(this.idleAnimationTimeout <= 0) {
+            this.idleAnimationTimeout = this.random.nextInt(40) + 80;
+            this.idleAnimationState.start(this.tickCount);
+        } else {
+            --this.idleAnimationTimeout;
+        }
+    }
+
+    @Override
+    protected void updateWalkAnimation(float pPartialTick) {
+        float f;
+        if(this.getPose() == Pose.FALL_FLYING) {
+            f = Math.min(pPartialTick * 6F, 1f);
+        } else {
+            f = 0f;
+        }
+
+        this.walkAnimation.update(f, 0.2f);
+    }
+
 
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
